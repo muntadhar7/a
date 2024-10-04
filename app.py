86db88455b850acdd62ce2fe61d8e5ac123815a8
muntadhar7 <mntr59@gmail.com>
2024-10-04 19:04:12 +0300

    aa

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import  Flask
-from ai import output
+from tkinter import Label
+
+from flask import Flask, render_template, FlaskForm
+from wtforms.fields.simple import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
+class MyForm(FlaskForm):
+    prompt = StringField(label="Prompt", validators=[DataRequired()])
+    submit = SubmitField()
+
 @app.route("/")
 def home():
-    return output
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
